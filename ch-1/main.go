cfaac789763dd32bdbaf54366118cd1e3c736deb
Aris <areskretindo@gmail.com>
2022-07-04 18:56:34 +0700

    map

--- ch-1/main.go
+++ ch-1/main.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
-var message = `Nama saya "John Wick".
+// use fmt for print anything
+
+// this section is about how to declare an variable
+var message = `Nama saya "John Wick".  
 Salam kenal.
 Mari belajar "Golang".`
 
@@ -12,17 +15,28 @@ var firstName string = "john"
 
 var lastName string
 
+var chicken map[string]int
+
+// end of  about how to declare an variable
+
+// this is how you run code in main function
 func main() {
-	lastName = "wick"
-	fmt.Println(message)
-	fmt.Printf("halo %s %s!\n", firstName, lastName)
 
-	var leftAndRight = left && right
-	fmt.Printf("left && right \t(%t) \n", leftAndRight)
+	chicken = map[string]int{}
+	chicken["januari"] = 50
+	chicken["februari"] = 40
 
-	var leftOrRight = left || right
-	fmt.Printf("left || right \t(%t) \n", leftOrRight)
+	main2()
+	// this is ho you call an function other than main
+	fmt.Println("januari", chicken["januari"]) // januari 50
+	fmt.Println("mei", chicken["mei"])         // mei 0 ,karena belum di definisikan
 
-	var leftReverse = !left
-	fmt.Printf("!left \t\t(%t) \n", leftReverse)
+}
+
+// end of how you run code in main function
+
+func main2() {
+
+	fmt.Println("\n and this is how you run a function outside main  \n")
+
 }
